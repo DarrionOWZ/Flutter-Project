47605ffa69040b2ae6ad5ec9ea84d636f2394eb3
DarrionOWZ <194994M@mymail.nyp.edu.sg>
2021-02-09 14:11:05 +0800

    Initial Commit

--- lib/CarPark.dart
+++ lib/CarPark.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:onemapsg/onemapsg.dart';
 
 class CarPark extends StatelessWidget {
-  Position CurrentPosition;
+  Position currentPosition;
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +13,7 @@ class CarPark extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           new Image.network(
-              'https://developers.onemap.sg/commonapi/staticmap/getStaticImage?layerchosen=default&lat=${CurrentPosition.latitude}&lng=${CurrentPosition.longitude}&zoom=16&height=512&width=512&polygons=&lines=&points=&color=&fillColor='),
+              'https://developers.onemap.sg/commonapi/staticmap/getStaticImage?layerchosen=default&lat=1.28666009534058&lng=103.837643678463&zoom=16&height=512&width=512&polygons=&lines=&points=&color=&fillColor='),
         ],
       ),
     );
--- lib/Price.dart
+++ lib/Price.dart
@@ -5,13 +5,108 @@ class Price extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(40.0),
       child: new Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          new Text(
-            'Price',
+          Text(
+            'Prices',
+            style: TextStyle(fontSize: 30.0),
           ),
-          Icon(Icons.directions_car),
+          SizedBox(height: 15.0),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 15.0, 0, 15.0),
+            decoration: BoxDecoration(
+                border: Border(
+                    bottom: BorderSide(width: 2.0, color: Colors.black),
+                    top: BorderSide(width: 2.0, color: Colors.black))),
+            child: Column(
+              children: [
+                Text('BLK 11/12/13 YORK HILL CAR PARK',
+                    style: TextStyle(fontSize: 20.0)),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  children: [
+                    Text('Short Term Parking',
+                        style: TextStyle(fontSize: 20.0)),
+                    SizedBox(
+                      width: 45,
+                    ),
+                    Text('WHOLE DAY', style: TextStyle(fontSize: 20.0))
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  children: [
+                    Text('Free Parking', style: TextStyle(fontSize: 20.0)),
+                    SizedBox(
+                      width: 75.0,
+                    ),
+                    Column(
+                      children: [
+                        Text('SUN & PH FR', style: TextStyle(fontSize: 20.0)),
+                        Text(
+                          ' 7AM-10.30PM',
+                          style: TextStyle(fontSize: 20.0),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15.0),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 15.0, 0, 15.0),
+            decoration: BoxDecoration(
+                border: Border(
+                    bottom: BorderSide(width: 2.0, color: Colors.black),
+                    top: BorderSide(width: 2.0, color: Colors.black))),
+            child: Column(
+              children: [
+                Text('BLK 747/752 YISHUN STREET 72',
+                    style: TextStyle(fontSize: 20.0)),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  children: [
+                    Text('Short Term Parking',
+                        style: TextStyle(fontSize: 20.0)),
+                    SizedBox(
+                      width: 45,
+                    ),
+                    Text('WHOLE DAY', style: TextStyle(fontSize: 20.0))
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  children: [
+                    Text('Free Parking', style: TextStyle(fontSize: 20.0)),
+                    SizedBox(
+                      width: 75.0,
+                    ),
+                    Column(
+                      children: [
+                        Text('SUN & PH FR', style: TextStyle(fontSize: 20.0)),
+                        Text(
+                          ' 7AM-10.30PM',
+                          style: TextStyle(fontSize: 20.0),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
